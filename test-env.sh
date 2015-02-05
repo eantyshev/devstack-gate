df9701a7ecf7ba75169e5fc5216d68202ba1efa1
Evgeny Antyshev <eantyshev@parallels.com>
2015-02-05 17:24:55 +0300

    temporary files

---
+++ test-env.sh
@@ -0,0 +1,9 @@
+export WORKSPACE=/opt/workspace
+export GIT_BASE=https://github.com
+export ZUUL_BRANCH=stable/juno
+export OVERRIDE_NOVA_PROJECT_BRANCH=master
+
+export DEVSTACK_GATE_NEUTRON=1
+export DEVSTACK_GATE_TEMPEST=1
+export DEVSTACK_GATE_VIRT_DRIVER=libvirt
+export DEVSTACK_GATE_LIBVIRT_TYPE=parallels
